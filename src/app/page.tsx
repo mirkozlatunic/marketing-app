c4dcf2f0abaedc58ef1ba362f0a227917e2cd546
Mirko Zlatunic <m.zlatunic@gmail.com>
2024-08-25 14:48:15 -0700

    setup fo header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,5 @@
 import Link from "next/link";
 
 export default function Home() {
-  return (
-    <div>
-      <div>
-        <Link href="/performance" />
-        Performance
-      </div>
-      <div>
-        <Link href="/reliability" />
-        Reliability
-      </div>
-      <div>
-        <Link href="/scale" />
-        Scale
-      </div>
-      Home Page
-    </div>
-  );
+  return <div>Home Page</div>;
 }
